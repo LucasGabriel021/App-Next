b58bb871f4e0dc3f091bc38b54a98a935587714c
Lucas Gabriel <lucasgabrielgomes33@gmail.com>
2026-01-06 14:09:12 -0300

    feat: criação da seção about

---
+++ app/(components)/about.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import Image from "next/image";
+import About1 from "../../public/about-1.png";
+import About2 from "../../public/about-2.png";
+import { Check, MapPin } from "lucide-react";
+import { WhatsappLogoIcon } from "@phosphor-icons/react/dist/ssr";
+
+export default function About() {
+  return (
+    <section className="bg-[#FDF6EC py-16">
+      <div className="container px-4 mx-auto">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
+
+          <div className="relative">
+            <div className="relative w-full h-[400px] rounded-3xl overflow-hidden">
+              <Image
+                src={About1}
+                alt="Foto doCachorro"
+                fill
+                quality={100}
+                priority
+                className="object-cover hover:scale-110 duration-300"
+              />
+            </div>
+            <div className="absolute w-40 h-40 right-4 -bottom-8 border-4 rounded-lg border-white">
+              <Image
+                src={About2}
+                alt="Foto doCachorro 2"
+                fill
+                quality={100}
+                priority
+              />
+            </div>
+          </div>
+
+          <div className="space-y-6 mt-10">
+            <h2 className="text-4xl font-bold">SOBRE</h2>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti,
+              voluptatem, obcaecati explicabo hic excepturi deserunt enim
+              recusandae reiciendis quod minima mollitia eveniet! Iusto autem
+              consequuntur reprehenderit, sapiente quas architecto suscipit?
+            </p>
+
+            <ul className="space-y-4">
+              <li className="flex items-center gap-2">
+                <Check className="text-red-500" />
+                Aberto desde 2006.
+              </li>
+              <li className="flex items-center gap-2">
+                <Check className="text-red-500" />
+                Equipe com mais de 10 veterinários.
+              </li>
+              <li className="flex items-center gap-2">
+                <Check className="text-red-500" />
+                Qualidade é nossa prioridade
+              </li>
+            </ul>
+            <div className="flex gap-2">
+              <a
+                href="#"
+                className="bg-[#E84C3D] text-white flex items-center justify-center w-fit gap-2 px-4 py-2 rounded-md"
+              >
+                <WhatsappLogoIcon className="w-5 h-5 text-white" />
+                Contato via WhatsApp
+              </a>
+              <a
+                href="#"
+                className="flex items-center justify-center w-fit gap-2 px-4 py-2"
+              >
+                <MapPin className="w-5 h-5 text-black" />
+                Endereço da loja
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
